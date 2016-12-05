50a9216a5f2002e5949507d1cc075ffcbb095b95
jumpsnack <kir0302@gmail.com>
2016-12-05 22:28:14 +0900

    New Code

--- RestView/RestView.ino
+++ RestView/RestView.ino
@@ -1,10 +1,20 @@
-#include "DHT.h"
+#include <Bridge.h>
 #include <Console.h>
 #include <Process.h>
+#include <YunClient.h>
+#include <YunServer.h>
+#include <DHT.h>
+#include <SPI.h>
 
-//define xively connect info
-#define APIKEY        "pzRV0MjPrEFfOT4zHAdSEkJwWvt9xjiOJvCJZkvYSoruMvgk"   // replace your xively api key here
-#define FEEDID        "46267484"                   // replace your xively feed ID
+void updateData();
+void sendData();
+
+const char* server = "210.125.31.34";
+//const char* server = "192.168.0.104";
+char buffer[64];
+
+YunClient client;
+String parametri = "";
 
 /**
    DHT SET
@@ -16,24 +26,33 @@ DHT dht(DHTPIN, DHTTYPE);
 /**
    IR SET
 */
-#define IRPIN 8
-int ir_state = 0, last_state = 0;
+#define IRPIN_1 4
+int ir_1_state = 0, last_1_state = 0;
+
+#define IRPIN_2 6
+int ir_2_state = 0, last_2_state = 0;
 
 /**
- * Button SET
+    PIR SET
+*/
+#define PIRPIN_1 8
+int pir_1_state = 0;
+
+#define PIRPIN_2 10
+int pir_2_state = 0;
+
+/**
+   Button SET
 */
 #define BTNPIN 9
 int btn_state = 0;
 
-
-// set up net client info:
-const unsigned long postingInterval = 6000;  //delay between updates to xively.com
-unsigned long lastRequest = 0;      // when you last made a request
-String dataString = "";
-
 static char tbuffer[6];
 static char hbuffer[6];
-static char irbuffer[2];
+static char ir1buffer[2];
+static char ir2buffer[2];
+static char pir1buffer[2];
+static char pir2buffer[2];
 static char btnbuffer[2];
 
 void setup()
@@ -41,46 +60,45 @@ void setup()
   Bridge.begin();
   Console.begin();
 
-  /**
-     IR SET
-  */
-  pinMode(IRPIN, INPUT);
-  digitalWrite(IRPIN, HIGH);
 
   /**
-   * Button SET
+      IR SET
+  */
+  pinMode(IRPIN_1, INPUT);
+  pinMode(IRPIN_2, INPUT);
+  pinMode(PIRPIN_1, INPUT);
+  pinMode(PIRPIN_2, INPUT);
+  digitalWrite(IRPIN_1, HIGH);
+  digitalWrite(IRPIN_2, HIGH);
+
+  /**
+     Button SET
   */
   pinMode(BTNPIN, INPUT);
 
-  while (!Console) {
-    ; // wait for Console port to connect.
-  }
 
+  while (!Console);
+  parametri = "cmd=clear";
+  sendData();
 }
 
 void loop()
 {
-  // get a timestamp so you can calculate reading and sending intervals:
-  long now = millis();
-
-  // if the sending interval has passed since your
-  // last connection, then connect again and send data:
-  if (now - lastRequest >= postingInterval) {
-    updateData();
-    sendData();
-    lastRequest = now;
-  }
+  updateData();
+  sendData();
 }
 
 void updateData() {
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   float h = dht.readHumidity();
   float t = dht.readTemperature();
-  ir_state = digitalRead(IRPIN);
+  
+  ir_1_state = digitalRead(IRPIN_1);
+  ir_2_state = digitalRead(IRPIN_2);
+  pir_1_state = digitalRead(PIRPIN_1);
+  pir_2_state = digitalRead(PIRPIN_2);
   btn_state = digitalRead(BTNPIN);
 
-  if (isnan(t) || isnan(h) || isnan(ir_state))
+  if (isnan(t) || isnan(h) || isnan(ir_1_state) || isnan(ir_2_state) || isnan(pir_1_state) || isnan(pir_2_state))
   {
     Console.println("\nFailed to read from DHT");
   }
@@ -93,76 +111,91 @@ void updateData() {
     Console.print("Temperature: ");
     Console.print(t);
     Console.println(" *C");
-    Console.print("IR: ");
-    Console.print(ir_state);
+    Console.print("IR 1 : ");
+    Console.print(ir_1_state);
     Console.print("\t");
-    Console.print("BTN: ");
-    Console.print(btn_state);
+    Console.print("IR 2 : ");
+    Console.println(ir_2_state);
+    Console.print("PIR 1 : ");
+    Console.print(pir_1_state);
+    Console.print("\t");
+    Console.print("PIR 2 : ");
+    Console.println(pir_2_state);
+    //Console.print("BTN: ");
+    //Console.print(btn_state);
     Console.print("\n");
 
     //convert the sensor data from float to string
-   // sprintf(irbuffer, "%d", ir_state);
-   //sprintf(btnbuffer, "%d", btn_state);
-   itoa(ir_state, irbuffer, 10);
-   itoa(btn_state, btnbuffer, 10);
+    // sprintf(irbuffer, "%d", ir_state);
+    //sprintf(btnbuffer, "%d", btn_state);
+    itoa(ir_1_state, ir1buffer, 10);
+    itoa(ir_2_state, ir2buffer, 10);
+    itoa(pir_1_state, pir1buffer, 10);
+    itoa(pir_2_state, pir2buffer, 10);
+    itoa(btn_state, btnbuffer, 10);
     dtostrf(t, 5, 2, tbuffer);
     dtostrf(h, 5, 2, hbuffer);
 
 
     //add Humidity:
-    dataString = "Humidity,";
-    dataString += hbuffer;
+    parametri = "1=";
+    parametri += "Humidity/";
+    parametri += hbuffer;
 
     // convert the readings to a String to send it:
-    dataString += "\nTemperature,";
-    dataString += tbuffer;
+    parametri += ",Temperature/";
+    parametri += tbuffer;
 
     //add IR, if ir == 0 : Full, ir == 1 : empty
-    dataString += "\nIR,";
-    dataString += irbuffer;
+    parametri += ",IR/";
+    parametri += ir1buffer;
+
+    parametri += ",PIR/";
+    parametri += pir1buffer;
 
     //add BTN
-    dataString += "\nBTN,";
-    dataString += btnbuffer;
+    parametri += ",BTN/";
+    parametri += btnbuffer;
+
+    //add Humidity:
+    parametri += "&2=";
+    parametri += "Humidity/";
+    parametri += hbuffer;
+
+    // convert the readings to a String to send it:
+    parametri += ",Temperature/";
+    parametri += tbuffer;
+
+    //add IR, if ir == 0 : Full, ir == 1 : empty
+    parametri += ",IR/";
+    parametri += ir2buffer;
+
+    parametri += ",PIR/";
+    parametri += pir2buffer;
+
+    //add BTN
+    parametri += ",BTN/";
+    parametri += btnbuffer;
   }
 }
 
-// this method makes a HTTP connection to the server:
 void sendData() {
-  // form the string for the API header parameter:
-  String apiString = "X-ApiKey: ";
-  apiString += APIKEY;
-
-  // form the string for the URL parameter:
-  String url = "https://api.xively.com/v2/feeds/";
-  url += FEEDID;
-  url += ".csv";
-
-  // Send the HTTP PUT request
-
-  // Call a command in MS14.
-  // Is better to declare the Process here, so when the
-  // sendData function finishes the resources are immediately
-  // released. Declaring it global works too, BTW.
-  Process xively;
-  Console.print("Sending data... ");
-  xively.begin("curl");
-  xively.addParameter("-k");
-  xively.addParameter("--request");
-  xively.addParameter("PUT");
-  xively.addParameter("--data");
-  xively.addParameter(dataString);
-  xively.addParameter("--header");
-  xively.addParameter(apiString);
-  xively.addParameter(url);
-  xively.run();
-  Console.println("done!\n\n");
-
-  // If there's incoming data from the net connection,
-  // send it out the Serial:
-  while (xively.available() > 0) {
-    char c = xively.read();
-    Console.write(c);
+  if (client.connect(server, 443)) {
+    Console.println("Sending data...");
+    delay(2500);
+    client.print("POST ");
+    client.print("/?");
+    client.print(parametri);
+    client.println(" HTTP / 1.1");
+    client.println("Connection: Close");
+    client.println();
+    Console.println("done!\n\n");
+  } else {
+    Console.println("connection failed");
+    //digitalWrite(led_rosso, HIGH);
+    delay(1000);
+  }
+  if (client.connected()) {
+    client.stop();   //disconnect from server
   }
 }
-
